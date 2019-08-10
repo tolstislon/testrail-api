47464ff2acb20f4a8135e658837eb9fbfd77d533
tolstislon <tolstislon@gmail.com>
2019-08-10 04:08:09 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+remove_artifacts() {
+  rm -rf ./dist
+  rm -rf ./build
+  rm -rf ./*.egg-info
+}
+
+if [[ $1 == "test" ]]; then
+  repository=testpypi
+else
+  repository=pypi
+fi
+
+echo env: ${repository}
+
+remove_artifacts
+
+# config file $HOME/.pypirc
+pipenv run python setup.py bdist_wheel
+twine upload --repository ${repository} dist/*
+
+remove_artifacts
+
+read -p "Press enter to continue"

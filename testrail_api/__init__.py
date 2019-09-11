d8cbe3c7b8a103c070e2a926bae76a5332b8d974
tolstislon <tolstislon@gmail.com>
2019-09-11 11:22:14 +0300

    add logging

--- testrail_api/__init__.py
+++ testrail_api/__init__.py
@@ -1,5 +1,5 @@
 from ._testrail_api import TestRailAPI
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 
 __all__ = ['TestRailAPI']
--- testrail_api/_session.py
+++ testrail_api/_session.py
@@ -1,12 +1,15 @@
+import logging
 from pathlib import Path
 
 import requests
 
 from ._enums import METHODS
 
+log = logging.getLogger(__name__)
+
 
 class Session:
-    _user_agent = 'Python TestRail API v: 1.2'
+    _user_agent = 'Python TestRail API v: 1.3'
 
     def __init__(self, base_url: str, user: str, password: str, **kwargs):
         """
@@ -21,15 +24,29 @@ class Session:
         self.__timeout = kwargs.get('timeout', 30)
         self.__session = requests.Session()
         self.__session.headers['User-Agent'] = self._user_agent
-        self.__session.headers['Content-Type'] = 'application/json'
         self.__session.headers.update(kwargs.get('headers', {}))
         self.__session.verify = kwargs.get('verify', True)
+        log.info(
+            'Create Session{url: %s, user: %s, password: ***, timeout: %s, headers: %s, verify: %s}',
+            base_url, user, self.__timeout, self.__session.headers, self.__session.verify
+        )
         self.__session.auth = (user, password)
+
+    def __del__(self):
+        self.__session.close()
 
     def request(self, method: METHODS, src: str, **kwargs):
         """Base request method"""
         url = f'{self.__base_url}{src}'
+        if not src.startswith('add_attachment'):
+            headers = kwargs.setdefault('headers', {})
+            headers.update({'Content-Type': 'application/json'})
+
         response = self.__session.request(method=method.value, url=url, timeout=self.__timeout, **kwargs)
+
+        log.debug('Response header: %s', response.headers)
+        log.debug('Response body: %s', response.text)
+
         if 'json' in response.headers.get('Content-Type', ''):
             return response.json()
         else:
@@ -37,5 +54,5 @@ class Session:
 
     def attachment_request(self, method: METHODS, src: str, file: Path, **kwargs):
         """"""
-        return self.request(method, src, files={'file': file.open('rb')},
-                            headers={'Content-Type': 'multipart/form-data'}, **kwargs)
+        with file.open('rb') as attachment:
+            return self.request(method, src, files={'attachment': attachment}, **kwargs)

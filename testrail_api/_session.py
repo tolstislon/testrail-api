e3cc6898bb41fc11f92b27d4a085f5c9869b2e4d
tolstislon <tolstislon@gmail.com>
2021-09-06 21:31:44 +0300

    fix black style

--- testrail_api/_session.py
+++ testrail_api/_session.py
@@ -26,14 +26,14 @@ class Session:
     _user_agent = "Python TestRail API v: {}".format(__version__)
 
     def __init__(
-            self,
-            url: Optional[str] = None,
-            email: Optional[str] = None,
-            password: Optional[str] = None,
-            exc: bool = False,
-            rate_limit: bool = True,
-            warn_ignore: bool = False,
-            **kwargs
+        self,
+        url: Optional[str] = None,
+        email: Optional[str] = None,
+        password: Optional[str] = None,
+        exc: bool = False,
+        rate_limit: bool = True,
+        warn_ignore: bool = False,
+        **kwargs
     ) -> None:
         """
         :param url:
@@ -160,9 +160,9 @@ class Session:
                 logger.error("%s", err, exc_info=True)
                 raise
             if (
-                    self._rate_limit
-                    and response.status_code == RATE_LIMIT_STATUS_CODE
-                    and count < self.__exc_iterations - 1
+                self._rate_limit
+                and response.status_code == RATE_LIMIT_STATUS_CODE
+                and count < self.__exc_iterations - 1
             ):
                 time.sleep(int(response.headers.get("retry-after", self.__retry)))
                 continue
@@ -174,7 +174,7 @@ class Session:
         return path if isinstance(path, Path) else Path(path)
 
     def attachment_request(
-            self, method: METHODS, src: str, file: Union[Path, str], **kwargs
+        self, method: METHODS, src: str, file: Union[Path, str], **kwargs
     ):
         """Send attach"""
         file = self._path(file)
@@ -182,7 +182,7 @@ class Session:
             return self.request(method, src, files={"attachment": attachment}, **kwargs)
 
     def get_attachment(
-            self, method: METHODS, src: str, file: Union[Path, str], **kwargs
+        self, method: METHODS, src: str, file: Union[Path, str], **kwargs
     ) -> Path:
         """Downloads attach"""
         file = self._path(file)

5cf41cdd93567e565336c0a52d7864c085838ea8
tolstislon <tolstislon@gmail.com>
2025-12-30 12:05:29 +0300

    get_users added offset and limit

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,25 @@
 import json
 
+import pytest
 import responses
+
+
+def get_users(req):
+    return (
+        200,
+        {},
+        json.dumps(
+            {
+                "offset": int(req.params.get("offset", 0)),
+                "limit": int(req.params.get("limit", 250)),
+                "size": 2,
+                "users": [
+                    {"email": "testrail@ff.com", "id": 1, "name": "John Smith", "is_active": True},
+                    {"email": "testrail1@ff.com", "id": 2, "name": "Jane Smith", "is_active": True},
+                ],
+            }
+        ),
+    )
 
 
 def test_get_user(api, mock, url):
@@ -24,32 +43,22 @@ def test_get_user_by_email(api, mock, url):
     assert response["email"] == email
 
 
-def test_get_users(api, mock, url):
-    mock.add_callback(
-        responses.GET,
-        url("get_users/15"),
-        lambda _: (
-            200,
-            {},
-            json.dumps([{"email": "testrail@ff.com", "id": 1, "name": "John Smith", "is_active": True}]),
-        ),
-    )
-    response = api.users.get_users(15)
-    assert response[0]["name"] == "John Smith"
+@pytest.mark.parametrize(("offset", "limit"), ((None, None), (0, 250), (2, 14)))
+def test_get_users(api, mock, url, offset, limit):
+    mock.add_callback(responses.GET, url("get_users/15"), get_users)
+    response = api.users.get_users(15, offset=offset, limit=limit)
+    assert response["offset"] == (offset if offset is not None else 0)
+    assert response["limit"] == (limit if limit is not None else 250)
+    assert response["users"][0]["name"] == "John Smith"
 
 
-def test_get_users_no_project_id(api, mock, url):
-    mock.add_callback(
-        responses.GET,
-        url("get_users"),
-        lambda _: (
-            200,
-            {},
-            json.dumps([{"email": "testrail@ff.com", "id": 1, "name": "John Smith", "is_active": True}]),
-        ),
-    )
-    response = api.users.get_users()
-    assert response[0]["name"] == "John Smith"
+@pytest.mark.parametrize(("offset", "limit"), ((None, None), (0, 250), (2, 14)))
+def test_get_users_no_project_id(api, mock, url, offset, limit):
+    mock.add_callback(responses.GET, url("get_users"), get_users)
+    response = api.users.get_users(offset=offset, limit=limit)
+    assert response["offset"] == (offset if offset is not None else 0)
+    assert response["limit"] == (limit if limit is not None else 250)
+    assert response["users"][0]["name"] == "John Smith"
 
 
 def test_get_current_user(api, mock, url):
